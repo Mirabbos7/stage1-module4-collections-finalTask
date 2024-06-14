6ed9616a089292139dec5fb982405c93762fd0b3
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-14 16:45:11 +0500

    Completed 3 classes

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -6,7 +6,7 @@ public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
         Set<String> classes = new HashSet<>();
         for (List<String> subject: timetable.values()){
-            classes.add(subject.toString());
+            classes.addAll(subject);
         }
         return classes;
     }
